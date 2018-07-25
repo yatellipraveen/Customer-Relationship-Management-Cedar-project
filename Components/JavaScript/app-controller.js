36cd40a235a9161eb53e2ec99a24a274b1d1507a
yatellipraveen <yatellipraveen@gmail.com>
2018-07-25 09:11:16 +0530

    Styles.css Added

--- Components/JavaScript/app-controller.js
+++ Components/JavaScript/app-controller.js
@@ -2,17 +2,19 @@ var app=angular.module("CRMApp",[]);
 app.controller("CRMAppctrl",function($scope){
 	
 	$scope.pendinglist=[{name:'Nani',phone:'9564235421',campaign:'Lost Debit Card',status:'Pending'},
-	{name:'Charan',phone:'9564287634',campaign:'Lost Debit Card',status:'Pending'},
+	{name:'Charan',phone:'9564287634',campaign:'Request return',status:'Pending'},
 	{name:'Mark',phone:'9706835421',campaign:'Update Aadhar',status:'Pending'},
-	{name:'Aditya',phone:'9567768521',campaign:'Lost Debit Card',status:'Pending'},
+	{name:'Aditya',phone:'9567768521',campaign:'Request return',status:'Pending'},
 	{name:'Kiran',phone:'9564235421',campaign:'Lost Debit Card',status:'Pending'},
 	{name:'Aliza',phone:'8760975421',campaign:'Update Aadhar',status:'Pending'},
 	{name:'Sharma',phone:'7854305421',campaign:'Update Aadhar',status:'Pending'},
-	{name:'Nagaraj',phone:'7865485421',campaign:'Lost Debit Card',status:'Pending'},
+	{name:'Nagaraj',phone:'7865485421',campaign:'Request return',status:'Pending'},
 	{name:'Shruthi',phone:'9768965478',campaign:'Lost Debit Card',status:'Pending'},
 	{name:'Puja',phone:'8756506413',campaign:'Update Aadhar',status:'Pending'}];
 
-	$scope.completedlist=[{name:'Puja',phone:'8756506413',campaign:'Lost Debit Card',status:'Completed'}];
+	$scope.completedlist=[{name:'Nilofer',phone:'8756506413',campaign:'Lost Debit Card',status:'Completed'},
+	{name:'Aniket',phone:'7595468635',campaign:'Lost Debit Card',status:'Completed'},
+	{name:'Paul',phone:'9764893527',campaign:'Update Aadhar',status:'Completed'}];
 
 	
 	 $scope.moveToCompletedList = function(index) {
